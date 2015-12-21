57239b1f5dba26d809f40ba8e3758a561f3207b5
linux_china <linux_china@hotmail.com>
2015-12-21 11:53:53 +0800

    auth zuul filter

---
+++ zuul-proxy/src/main/java/org/mvnsearch/filter/AuthFilter.java
@@ -0,0 +1,43 @@
+package org.mvnsearch.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.filters.ProxyRouteLocator;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UrlPathHelper;
+
+/**
+ * auth zuul filter
+ *
+ * @author linux_china
+ */
+@Component
+public class AuthFilter extends ZuulFilter {
+    private UrlPathHelper urlPathHelper = new UrlPathHelper();
+    private ProxyRouteLocator routeLocator;
+
+    @Autowired
+    public AuthFilter(ProxyRouteLocator routeLocator) {
+        this.routeLocator = routeLocator;
+    }
+
+    public String filterType() {
+        return "pre";
+    }
+
+    public int filterOrder() {
+        return 1;
+    }
+
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        final String requestURI = this.urlPathHelper.getPathWithinApplication(ctx.getRequest());
+        ProxyRouteLocator.ProxyRouteSpec route = this.routeLocator.getMatchingRoute(requestURI);
+        return null;
+    }
+}
