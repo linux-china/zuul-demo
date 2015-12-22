f9b89f9728607689aca1dac35285315afe8a9f7f
linux_china <linux_china@hotmail.com>
2015-12-22 08:12:46 +0800

    add @EnableFeignClients to make rest template auto load balance

--- zuul-proxy/src/main/java/org/mvnsearch/ZuulProxyDemoApplication.java
+++ zuul-proxy/src/main/java/org/mvnsearch/ZuulProxyDemoApplication.java
@@ -3,11 +3,13 @@ package org.mvnsearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableFeignClients
 public class ZuulProxyDemoApplication {
 
     public static void main(String[] args) {
