83246f12bc70e20808d1320b810a47ad7a839d05
linux_china <linux_china@hotmail.com>
2015-12-22 08:12:19 +0800

    add rest template support

--- zuul-proxy/src/main/java/org/mvnsearch/controller/PortalController.java
+++ zuul-proxy/src/main/java/org/mvnsearch/controller/PortalController.java
@@ -1,7 +1,14 @@
 package org.mvnsearch.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.RestTemplateCustomizer;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 /**
  * portal controller
@@ -10,9 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class PortalController {
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping("/")
     public String index() {
-        return "hello";
+        ResponseEntity<String> entity = restTemplate.getForEntity("http://user-service/user/show", String.class);
+        return entity.getBody();
     }
 }
