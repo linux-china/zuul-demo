581391c9bdabaa7c0a67626d0dc4a93d1844e055
linux_china <linux_china@hotmail.com>
2016-05-12 15:15:12 +0800

    add restTemplate bean

--- zuul-proxy/src/main/java/org/mvnsearch/ZuulProxyDemoApplication.java
+++ zuul-proxy/src/main/java/org/mvnsearch/ZuulProxyDemoApplication.java
@@ -3,8 +3,11 @@ package org.mvnsearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -14,5 +17,11 @@ public class ZuulProxyDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulProxyDemoApplication.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
